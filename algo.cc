4773dbb0a954e4fa96f4ad50ac58e6d043434176
Nick Smith <nick.smith@cern.ch>
2020-04-27 00:44:06 +0000

    FM demod in cpp, save to file

--- algo.cc
+++ algo.cc
@@ -12,24 +12,33 @@ Buffer<int32_t, lowpass_fir_taps.size() + 1> downconverted_i;
 Buffer<int32_t, lowpass_fir_taps.size() + 1> downconverted_q;
 Buffer<int32_t, lowpass_fir_taps.size() + 1> downsample1_i;
 Buffer<int32_t, lowpass_fir_taps.size() + 1> downsample1_q;
-Buffer<int32_t, lowpass_fir_taps.size() + 1> downsample2_i;
-Buffer<int32_t, lowpass_fir_taps.size() + 1> downsample2_q;
+constexpr std::array<int32_t, 15> lowdiff_fir_taps {
+  -2,    24,  -120,   338,  -489,  -137,  3300,     0, -3300, 137,   489,  -338,   120,   -24,     2
+};
+constexpr std::array<int32_t, 15> lowdelay_fir_taps {
+  -2,    8,  -25,   28,  106, -528, 1117, 3462, 1117, -528,  106, 28,  -25,    8,   -2
+};
+constexpr size_t lowdiff_fir_frac{4096};
+Buffer<int32_t, lowdiff_fir_taps.size() + 1> downsample2_i;
+Buffer<int32_t, lowdiff_fir_taps.size() + 1> downsample2_q;
 uint32_t sum_mag{0};
 uint32_t n_mag{0};
 
-std::tuple<uint32_t, uint32_t> algo(RTLDevice& device, Oscillator& oscillator) {
+std::tuple<uint32_t, uint32_t> algo(RTLDevice& device, Oscillator& oscillator, Buffer<int32_t, 64*1024>& output) {
   sum_mag = 0;
   n_mag = 0;
   size_t nqueue = std::min(16384u, device.output.used() / 2);
   while ( nqueue-- > 0 ) {
-    auto iq = oscillator.pop();
+    auto iq_osc = oscillator.pop();
+    int32_t i_in = (int32_t) device.output.pop() - 127;
+    int32_t q_in = (int32_t) device.output.pop() - 127;
     // 7 * 10 -> 10
-    downconverted_i.push((device.output.pop() * iq[0]) / 128);
-    downconverted_q.push((device.output.pop() * iq[1]) / 128);
+    downconverted_i.push((i_in * iq_osc[0]) / 128);
+    downconverted_q.push((q_in * iq_osc[1]) / 128);
 
     if ( downconverted_i.full() ) {
-      uint32_t firsum_i{0};
-      uint32_t firsum_q{0};
+      int32_t firsum_i{0};
+      int32_t firsum_q{0};
       for (size_t i=0; i < lowpass_fir_taps.size(); ++i) {
         firsum_i += lowpass_fir_taps[i] * downconverted_i.get(i);
         firsum_q += lowpass_fir_taps[i] * downconverted_q.get(i);
@@ -40,8 +49,8 @@ std::tuple<uint32_t, uint32_t> algo(RTLDevice& device, Oscillator& oscillator) {
       downconverted_q.advance(lowpass_fir_div);
 
       if ( downsample1_i.full() ) {
-        uint32_t firsum_i{0};
-        uint32_t firsum_q{0};
+        int32_t firsum_i{0};
+        int32_t firsum_q{0};
         for (size_t i=0; i < lowpass_fir_taps.size(); ++i) {
           firsum_i += lowpass_fir_taps[i] * downsample1_i.get(i);
           firsum_q += lowpass_fir_taps[i] * downsample1_q.get(i);
@@ -51,7 +60,21 @@ std::tuple<uint32_t, uint32_t> algo(RTLDevice& device, Oscillator& oscillator) {
         downsample1_i.advance(lowpass_fir_div);
         downsample1_q.advance(lowpass_fir_div);
 
-        if ( not downsample2_i.empty() ) {
+        if ( downsample2_i.full() ) {
+          int32_t i_diff{0};
+          int32_t q_diff{0};
+          int32_t i_delay{0};
+          int32_t q_delay{0};
+          for (size_t i=0; i < lowdiff_fir_taps.size(); ++i) {
+            i_diff += lowdiff_fir_taps[i] * downsample2_i.get(i);
+            q_diff += lowdiff_fir_taps[i] * downsample2_q.get(i);
+            i_delay += lowdelay_fir_taps[i] * downsample2_i.get(i);
+            q_delay += lowdelay_fir_taps[i] * downsample2_q.get(i);
+          }
+          // 10 * 12 -> 22 fractional bit output
+          int32_t dphi = (i_delay * q_diff - i_diff * q_delay);
+          output.push(dphi);
+
           auto i = downsample2_i.pop();
           auto q = downsample2_q.pop();
           sum_mag += (i*i + q*q) / 1024;
--- rtl.cc
+++ rtl.cc
@@ -1,4 +1,5 @@
 #include <chrono>
+#include <fstream>
 #include <iostream>
 #include <thread>
 #include <mutex>
@@ -6,7 +7,7 @@
 #include "nco.h"
 #include "buffer.h"
 
-std::tuple<uint32_t, uint32_t> algo(RTLDevice& device, Oscillator& oscillator);
+std::tuple<uint32_t, uint32_t> algo(RTLDevice& device, Oscillator& oscillator, Buffer<int32_t, 64*1024>& output);
 
 int main(void) {
   std::cout << "Device count: " << RTLDevice::count() << std::endl;
@@ -21,6 +22,28 @@ int main(void) {
     device.setSampleRate(f_samp);
     device.setGainMode(RTLDevice::GAIN_AUTO);
 
+    Buffer<int32_t, 64*1024> output;
+    std::ofstream fout;
+    fout.open("audio.dat", std::ofstream::out | std::ofstream::trunc | std::ofstream::binary);
+    bool output_open{true};
+    std::mutex output_mutex;
+    std::condition_variable output_cv;
+    std::thread output_thread(
+      [&fout, &output, &output_open, &output_mutex, &output_cv]
+      {
+        while ( true ) {
+          {
+            std::unique_lock<std::mutex> lock(output_mutex);
+            output_cv.wait(lock, [&output, &output_open]{return (output.used() > 16*1024) or not output_open;});
+            while ( not output.empty() ) {
+              fout << output.pop();
+            }
+          }
+          fout.flush();
+          if ( not output_open ) break;
+        }
+      }
+    );
 
     device.start();
     auto tic = std::chrono::steady_clock::now();
@@ -28,17 +51,19 @@ int main(void) {
     uint32_t n_mag{0};
     for (size_t it=0; it<5000; ++it) {
       {
+        std::lock_guard<std::mutex> olock(output_mutex);
         std::unique_lock<std::mutex> lock(device.output_mutex);
         device.output_cv.wait(lock, [&device]{return device.output.used() >= 2;});
         if ( it % 500 == 0 ) {
           std::cout << "Receiving: buffer @ " << device.output.used() << " bytes...";
         }
-        std::tie(sum_mag, n_mag) = algo(device, oscillator);
+        std::tie(sum_mag, n_mag) = algo(device, oscillator, output);
         if ( it % 500 == 0 ) {
           float magavg = (n_mag > 0) ? sum_mag / (float) n_mag : 0.;
           std::cout << " nds: " << n_mag << ", avg mag: " << magavg << std::endl;
         }
       }
+      output_cv.notify_one();
     }
     device.stop();
     auto toc = std::chrono::steady_clock::now();
@@ -47,6 +72,13 @@ int main(void) {
     std::cout << "Bytes read: " << device.bytesRead() << std::endl;
     std::cout << "SPS: " << device.bytesRead() / 2 / diff.count() << std::endl;
     std::cout << "Dropped: " << device.output.dropped() << std::endl;
+    {
+      std::lock_guard<std::mutex> olock(output_mutex);
+      output_open = false;
+    }
+    output_cv.notify_one();
+    output_thread.join();
+    fout.close();
   }
   return 0;
 }
